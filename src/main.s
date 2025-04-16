035064e50b1671541f744e1e5d2501b2940e9c3c
root <root@DESKTOP-DLQ3CU5.>
2025-04-16 19:53:21 +0100

    Add intruction sets. Update .s

--- src/main.s
+++ src/main.s
@@ -4,8 +4,35 @@
                 ; entry point (tells the linker to use this as the 
                 ; reset vector).
 
+.equ PORTB, 0x05
+.equ DDRB,  0x04
+
 __start:
     RJMP main   ; Jump to your main code   
 
+delay:
+    DEC R16
+    BREQ delay
+
+; Register setup routine
+setup:
+    SBI DDRB, 5 ; Set the bit 5 of DDRB register (GPIO 13 set to OUTPUT)
+
+; Loop routine
+loop:
+    SBI PORTB, 5 ; Set the bit 5 of PORTB register (GPIO 13 set to HIGH)
+
+    ; delay
+    LDI R16, 0xFF
+    RJMP delay
+
+    CBI PORTB, 5 ; Clear the bit 5 of PORTB register (GPIO 13 set to HIGH)
+
+    ; Add delay
+
+    RJMP loop   ; Infinite loop to keep the MCU alive
+
+; Main routine
 main:
-    RJMP main   ; Infinite loop to keep the MCU alive
+    RJMP setup
+    RJMP loop
